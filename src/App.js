02f9e2862c4789cd2c2d0a937ae7cdef3506852b
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2021-09-15 19:18:57 -0300

    :zap: exercicio dificilll
    aiai

--- src/App.js
+++ src/App.js
@@ -1,64 +1,38 @@
-import React, { Component } from 'react';
+import React, { useContext, useEffect } from 'react';
 
+import LastUpdate from './components/LastUpdate';
 import Posts from './components/Posts';
+import RefreshButton from './components/RefreshButton';
 import Selector from './components/Selector';
 import { Context } from './components/RedditContext';
 
-class App extends Component {
-  componentDidMount() {
-    const { fetchPosts } = this.context;
+const App = () => {
+  const {
+    fetchPosts,
+    selectedSubreddit,
+    postsBySubreddit,
+    isFetching,
+  } = useContext(Context);
+
+  useEffect(() => {
     fetchPosts();
-  }
+  }, []);
 
-  renderLastUpdatedAt() {
-    const { selectedSubreddit, postsBySubreddit } = this.context;
-    const { lastUpdated } = postsBySubreddit[selectedSubreddit];
+  const { items: posts = [] } = postsBySubreddit[selectedSubreddit];
+  const isEmpty = posts.length === 0;
 
-    if (!lastUpdated) return null;
-
-    return (
-      <span>
-        {`Last updated at ${new Date(lastUpdated).toLocaleTimeString()}.`}
-      </span>
-    );
-  }
-
-  renderRefreshButton() {
-    const { isFetching, refreshSubreddit } = this.context;
-
-    if (isFetching) return null;
-
-    return (
-      <button
-        type="button"
-        onClick={(event) => refreshSubreddit(event)}
-        disabled={isFetching}
-      >
-        Refresh
-      </button>
-    );
-  }
-
-  render() {
-    const { selectedSubreddit, postsBySubreddit, isFetching } = this.context;
-    const { items: posts = [] } = postsBySubreddit[selectedSubreddit];
-    const isEmpty = posts.length === 0;
-
-    return (
+  return (
+    <div>
+      <Selector />
       <div>
-        <Selector />
-        <div>
-          {this.renderLastUpdatedAt()}
-          {this.renderRefreshButton()}
-        </div>
-        {isFetching && <h2>Loading...</h2>}
-        {!isFetching && isEmpty && <h2>Empty.</h2>}
-        {!isFetching && !isEmpty && <Posts />}
+        <LastUpdate />
+        <RefreshButton />
       </div>
-    );
-  }
-}
-
-App.contextType = Context;
+      {isFetching && <h2>Loading...</h2>}
+      {!isFetching && isEmpty && <h2>Empty.</h2>}
+      {!isFetching && !isEmpty && <Posts />}
+    </div>
+  );
+};
 
 export default App;
